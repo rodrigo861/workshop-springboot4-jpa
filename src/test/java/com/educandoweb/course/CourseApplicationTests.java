5aa1f990ec917e03548532bb02bd3cab348d27ba
rodrigo861 <rodrigojnooli@gmail.com>
2026-04-13 08:56:38 -0300

    User entity and Resource

--- src/test/java/com/educandoweb/course/CourseApplicationTests.java
+++ src/test/java/com/educandoweb/course/CourseApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class ApplicationTests {
+class CourseApplicationTests {
 
 	@Test
 	void contextLoads() {
